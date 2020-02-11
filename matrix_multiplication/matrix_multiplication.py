5f37008bba8cb9505f5d64ebe508e29b7ecc4cc3
Shineun Yoon <syoon@dhcp-wifi-8021x-155-41-124-201.bu.edu>
2020-02-11 17:25:10 -0500

    Added Matrix Multiplication Code

--- matrix_multiplication/matrix_multiplication.py
+++ matrix_multiplication/matrix_multiplication.py
@@ -9,7 +9,10 @@
 #                        dev@babyMRI.org
 #
 
-
+from __future__ import division
+from numba import cuda, float32
+import numpy
+import math
 import os
 import sys
 sys.path.append(os.path.dirname(__file__))
@@ -17,12 +20,19 @@ sys.path.append(os.path.dirname(__file__))
 # import the Chris app superclass
 from chrisapp.base import ChrisApp
 
+# Controls threads per block and shared memory usage.
+# The computation will be done on blocks of TPBxTPB elements.
+TPB = 16
 
 Gstr_title = """
-
-Generate a title from 
-http://patorjk.com/software/taag/#p=display&f=Doom&t=matrix_multiplication
-
+                 _        _                        _ _   _       _ _           _   _             
+                | |      (_)                      | | | (_)     | (_)         | | (_)            
+ _ __ ___   __ _| |_ _ __ ___  __  _ __ ___  _   _| | |_ _ _ __ | |_  ___ __ _| |_ _  ___  _ __  
+| '_ ` _ \ / _` | __| '__| \ \/ / | '_ ` _ \| | | | | __| | '_ \| | |/ __/ _` | __| |/ _ \| '_ \ 
+| | | | | | (_| | |_| |  | |>  <  | | | | | | |_| | | |_| | |_) | | | (_| (_| | |_| | (_) | | | |
+|_| |_| |_|\__,_|\__|_|  |_/_/\_\ |_| |_| |_|\__,_|_|\__|_| .__/|_|_|\___\__,_|\__|_|\___/|_| |_|
+                              ______                      | |                                    
+                             |______|                     |_|                                    
 """
 
 Gstr_synopsis = """
@@ -92,14 +102,14 @@ class matrix_multiplication(ChrisApp):
     """
     An app to ....
     """
-    AUTHORS                 = 'FNNDSC (yse@bu.edu)'
+    AUTHORS                 = 'FNNDSC (emslade@bu.edu, jeff0410@bu.edu, haoyangw@bu.edu, kefan29@bu.edu, yse@bu.edu)'
     SELFPATH                = os.path.dirname(os.path.abspath(__file__))
     SELFEXEC                = os.path.basename(__file__)
     EXECSHELL               = 'python3'
     TITLE                   = 'A ChRIS plugin app'
     CATEGORY                = ''
     TYPE                    = 'ds'
-    DESCRIPTION             = 'An app to ...'
+    DESCRIPTION             = 'An app to test matrix multiplication on GPU'
     DOCUMENTATION           = 'http://wiki'
     VERSION                 = '0.1'
     ICON                    = '' # url of an icon image
@@ -132,12 +142,74 @@ class matrix_multiplication(ChrisApp):
         Use self.add_argument to specify a new app argument.
         """
 
+    @cuda.jit
+    def fast_matmul(A, B, C):
+        """
+        Perform matrix multiplication of C = A * B
+        Each thread computes one element of the result matrix C
+        """
+
+        # Define an array in the shared memory
+        # The size and type of the arrays must be known at compile time
+        sA = cuda.shared.array(shape=(TPB, TPB), dtype=float32)
+        sB = cuda.shared.array(shape=(TPB, TPB), dtype=float32)
+
+        x, y = cuda.grid(2)
+        
+        tx = cuda.threadIdx.x
+        ty = cuda.threadIdx.y
+        
+        if x >= C.shape[0] and y >= C.shape[1]:
+            # Quit if (x, y) is outside of valid C boundary
+            return
+
+        # Each thread computes one element in the result matrix.
+        # The dot product is chunked into dot products of TPB-long vectors.
+        tmp = 0.
+        for i in range(int(A.shape[1] / TPB)):
+            # Preload data into shared memory
+            sA[tx, ty] = A[x, ty + i * TPB]
+            sB[tx, ty] = B[tx + i * TPB, y]
+
+            # Wait until all threads finish preloading
+            cuda.syncthreads()
+
+            # Computes partial product on the shared memory
+            for j in range(TPB):
+                tmp += sA[tx, j] * sB[j, ty]
+
+            # Wait until all threads finish computing
+            cuda.syncthreads()
+
+        C[x, y] = tmp
+
     def run(self, options):
         """
         Define the code to be run by this plugin app.
         """
         print(Gstr_title)
         print('Version: %s' % self.get_version())
+        fast_matmul(A, B, C)
+
+        # The data array
+        A = numpy.full((TPB*2, TPB*3), 3, numpy.float) # [32 x 48] matrix containing all 3's
+        B = numpy.full((TPB*3, TPB*1), 4, numpy.float) # [48 x 16] matrix containing all 4's
+
+        A_global_mem = cuda.to_device(A)
+        B_global_mem = cuda.to_device(B)
+        C_global_mem = cuda.device_array((TPB*2, TPB*1)) # [32 x 16] matrix result
+
+        # Configure the blocks
+        threadsperblock = (TPB, TPB)
+        blockspergrid_x = int(math.ceil(A.shape[0] / threadsperblock[1]))
+        blockspergrid_y = int(math.ceil(B.shape[1] / threadsperblock[0]))
+        blockspergrid = (blockspergrid_x, blockspergrid_y)
+
+        # Start the kernel 
+        fast_matmul[blockspergrid, threadsperblock](A_global_mem, B_global_mem, C_global_mem)
+        res = C_global_mem.copy_to_host()
+
+        print(res)
 
     def show_man_page(self):
         """
